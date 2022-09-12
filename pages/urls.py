aa2e1be8b609213fa6bc5712c83855c1d9b4fd31
matias cavieres <imatiascavieres@outlook.com>
2022-09-11 20:39:29 -0400

    Se agrego Sign up

--- pages/urls.py
+++ pages/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import HomePageView, AboutPageView , LoginPageView
 
 urlpatterns = [
-    path("", HomePageView.as_view(), name="home"),
+    path("", HomePageView.as_view(), name="base"),
     path("about/", AboutPageView.as_view(), name="about"),
     path("login/", LoginPageView.as_view(), name="login"),
 ]
--- pages/views.py
+++ pages/views.py
@@ -8,7 +8,7 @@ def homePageView(request):
 
 
 class HomePageView(TemplateView):
-    template_name = "home.html"
+    template_name = "base.html"
 
 class AboutPageView(TemplateView):
     template_name = "about.html"
